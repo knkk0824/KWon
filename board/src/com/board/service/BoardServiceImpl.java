32f378cc60507144cd8a26d4218c8be6ed03411b
java-19 <java-19@java-19-PC>
2017-11-09 15:32:31 +0900

    create board project

---
+++ board/src/com/board/service/BoardServiceImpl.java
@@ -0,0 +1,7 @@
+package com.board.service;
+
+import com.board.servlce.BoardService;
+
+public class BoardServiceImpl implements BoardService {
+
+}
---
+++ board/src/com/board/servlce/BoardService.java
@@ -0,0 +1,5 @@
+package com.board.servlce;
+
+public interface BoardService {
+
+}
